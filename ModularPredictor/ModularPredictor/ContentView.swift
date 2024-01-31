62bf366dad46b3efb00e307e4013a90cdd6a55f6
Robert Bates <thegreatbobini@gmail.com>
2024-01-31 11:51:04 -0800

    created modular predictor

---
+++ ModularPredictor/ModularPredictor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ModularPredictor
+//
+//  Created by Robert Bates on 1/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ModularPredictor/ModularPredictor/ModularPredictorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModularPredictorApp.swift
+//  ModularPredictor
+//
+//  Created by Robert Bates on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct ModularPredictorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
